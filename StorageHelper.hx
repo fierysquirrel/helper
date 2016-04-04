9209bf5b54c9166e7b14ee993964fc3f2b5452a2
Henry Fernández <henry@fierysquirrel.com>
2016-04-05 01:01:51 +0900

    - Adding storage support

--- StorageHelper.hx
+++ StorageHelper.hx
@@ -1,6 +1,8 @@
 package;
 
 import openfl.Assets;
+import openfl.net.SharedObject;
+import openfl.net.SharedObjectFlushStatus;
 
 /**
  * ...
@@ -17,5 +19,28 @@ class StorageHelper
 		xml = Xml.parse(str).firstElement();
 		
 		return xml;
+	}
+	
+	public static function SaveData(sharedObj : SharedObject) : Dynamic
+	{
+		// Prepare to save.. with some checks
+		#if ( cpp || neko )
+			// Android didn't wanted SharedObjectFlushStatus not to be a String
+			var flushStatus : SharedObjectFlushStatus = null;
+		#else
+			// Flash wanted it very much to be a String
+			var flushStatus : String = null;
+		#end
+
+		try 
+		{
+			flushStatus = sharedObj.flush() ;// Save the object
+		} 
+		catch ( e:Dynamic ) 
+		{
+			trace('couldn\'t write...');
+		}
+		
+		return flushStatus;
 	}
 }
